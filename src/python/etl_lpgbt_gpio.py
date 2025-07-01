bb6b8a79b40f1b5e7356e7c06d4783929388f243
Hayden Swanson <hayden_swanson22@yahoo.com>
2025-07-01 06:01:50 -0400

    udpates from first test

--- src/python/etl_lpgbt_gpio.py
+++ src/python/etl_lpgbt_gpio.py
@@ -6,16 +6,30 @@ from ..controllers.lpgbt_controller import *
 # 0x73
 # 0x60
 # 0x70
-lpgbt = lpgbt_chip("Master LPGBT")
+lpgbt = lpgbt_chip(
+    "Master LPGBT",
+    connection="~/mtd-emp-toolbox/mtd-daq/lpGBTv2_3_SO1_ceacmsfw_250603_1554_ETL/hls_connections.xml",
+    link=4,
+    lpgbt_address="0x73"
+    )
 
 # set pin 8 as an output
 # 0000 0001 0000 0000
-lpgbt.gpio_set_dir(0x100)
+print("#################")
+print("SETTING DIRECTION")
+lpgbt.gpio_set_dir(0xFFFF)
+print("#################\n")
+time.sleep(1)
+print("setting low")
+lpgbt.gpio_set_out(0x0)
+
 
 # set pin 8 as high
 # 0000 0001 0000 0000
-lpgbt.gpio_set_out(0x100)
-time.sleep(10)
-lpgbt.gpio_set_out(0x0)
+# print("*************SETTING HIGH************")
+# lpgbt.gpio_set_out(0x100)
+# time.sleep(10)
+# print("SETTING LOW")
+# time.sleep(5)
 
-print(lpgbt.gpio_get_out())
+#print(lpgbt.gpio_get_out())
--- src/python/etl_lpgbt_test.py
+++ src/python/etl_lpgbt_test.py
@@ -2,31 +2,37 @@ import time
 import sys
 from ..controllers.lpgbt_controller import *
 
+if "/home/cmx/mtd-emp-toolbox/chip_reformat/src" in sys.path:
+    sys.path.remove("/home/cmx/mtd-emp-toolbox/chip_reformat/src")
+
 for i in range(50):
-    lpgbt = lpgbt_chip("Master LPGBT")
-    #lpgbt = lpgbt_chip("LPGBT0", "CC1_T2")
+    try:
+        lpgbt = lpgbt_chip("Master LPGBT")
+        #lpgbt = lpgbt_chip("LPGBT0", "CC1_T2")
 
+        # TOGGLE DOWNLINK INTO UPLINK
+        lpgbt.write_reg(0x118, 0xC0)
+        time.sleep(0.1)
+        lpgbt.write_reg(0x118, 0)
 
-    # TOGGLE DOWNLINK INTO UPLINK
-    lpgbt.write_reg(0x118, 0xC0)
-    time.sleep(0.1)
-    lpgbt.write_reg(0x118, 0)
+        # HIGH SPEED DATA OUT INVERT, 
+        lpgbt.lpgbt_cont_.set_multiwrite(False) #write immediately to register
+        reg_addr = lpgbt.decode_reg_addr(lpgbt.CHIPCONFIG)
+        mask = lpgbt.CHIPCONFIG.HIGHSPEEDDATAOUTINVERT.bit_mask
+        lpgbt.lpgbt_cont_.lpgbt_com.set_link(reg_addr = reg_addr, mask = mask, lpgbt_addr = int(lpgbt.lpgbt_cont_.lpgbt_addr, 16))
+        lpgbt.lpgbt_cont_.set_multiwrite(True)  #cache writings until the first read is done
 
-    # HIGH SPEED DATA OUT INVERT, 
-    lpgbt.lpgbt_cont_.set_multiwrite(False) #write immediately to register
-    reg_addr = lpgbt.decode_reg_addr(lpgbt.CHIPCONFIG)
-    mask = lpgbt.CHIPCONFIG.HIGHSPEEDDATAOUTINVERT.bit_mask
-    lpgbt.lpgbt_cont_.lpgbt_com.set_link(reg_addr = reg_addr, mask = mask, lpgbt_addr = int(lpgbt.lpgbt_cont_.lpgbt_addr, 16))
-    lpgbt.lpgbt_cont_.set_multiwrite(True)  #cache writings until the first read is done
+        lpgbt.config_done() #POWERUP2
+        time.sleep(0.1)
+        # should have this method because it inherits from LpgbtV1 which inherits from Lpgbt
+        v1 = lpgbt.read_reg(0x1c5)
 
-    lpgbt.config_done() #POWERUP2
-    time.sleep(0.1)
-    # should have this method because it inherits from LpgbtV1 which inherits from Lpgbt
-    v1 = lpgbt.read_reg(0x1c5)
-
-    print("v1 is", v1)
-    if v1 == 0xa6:
-        break
-    else:
-        print("trying again...")
-        time.sleep(0.3)
+        print("v1 is", v1)
+        if v1 == 0xa6:
+            break
+        else:
+            print("trying again...")
+            time.sleep(0.3)
+    except:
+        print("failed but gonna try again")
+        time.sleep(1)
