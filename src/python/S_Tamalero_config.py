718d47a4376291f2a2377f2cbc5be03639cee43a
Hayden Swanson <70072888+hswanson13@users.noreply.github.com>
2025-07-11 15:29:15 +0200

    Update S_Tamalero_config.py
    copy paste swag

--- src/python/S_Tamalero_config.py
+++ src/python/S_Tamalero_config.py
@@ -10,34 +10,59 @@ from ..controllers.lpgbt_controller import *
 if "/home/cmx/mtd-emp-toolbox/chip_reformat/src" in sys.path:
     sys.path.remove("/home/cmx/mtd-emp-toolbox/chip_reformat/src")
 
+lpgbt = lpgbt_chip(
+    "Master LPGBT", 
+    connection="~/mtd-emp-toolbox/mtd-daq/lpGBTv2_3_SO1_ceacmsfw_250603_1554_ETL/hls_connections.xml")
+lpgbt.lpgbt_cont_.set_multiwrite(False)
+# try:
+#     lpgbt.init_lpgbt()
+# except Exception as e:
+#     print(f"Failed at lpgbt power up first read try: {e}")
+time.sleep(1)
+lpgbt.write_reg(0x03b, 0)
+lpgbt.write_reg(0x0f1, 0x50)
+lpgbt.write_reg(0x039, 0x7f)
+lpgbt.write_reg(0x037, 0)
+time.sleep(5)
+
+try:
+    print("0x{0:x}".format(
+        lpgbt.lpgbt_cont_.read_lpgbt_regs(int("0x1d9", 0), 1)[0])
+    )
+except Exception as e:
+    print(f"Failed to read:{e}")
+
 for i in range(50):
     try:
-        lpgbt = lpgbt_chip("Master LPGBT")
-        #lpgbt = lpgbt_chip("LPGBT0", "CC1_T2")
-
         # TOGGLE DOWNLINK INTO UPLINK
-        lpgbt.write_reg(0x118, 0xC0)
+        print("Toggling downlink into uplink")
+        time.sleep(0.01)
+        lpgbt.write_reg(0x128, 0xC0)#0xC0
         time.sleep(0.1)
-        lpgbt.write_reg(0x118, 0)
+        lpgbt.write_reg(0x128, 0)
+        # time.sleep(1)
 
         # HIGH SPEED DATA OUT INVERT, 
-        lpgbt.lpgbt_cont_.set_multiwrite(False) #write immediately to register
         reg_addr = lpgbt.decode_reg_addr(lpgbt.CHIPCONFIG)
         mask = lpgbt.CHIPCONFIG.HIGHSPEEDDATAOUTINVERT.bit_mask
         lpgbt.lpgbt_cont_.lpgbt_com.set_link(reg_addr = reg_addr, mask = mask, lpgbt_addr = int(lpgbt.lpgbt_cont_.lpgbt_addr, 16))
-        lpgbt.lpgbt_cont_.set_multiwrite(True)  #cache writings until the first read is done
 
         lpgbt.config_done() #POWERUP2
-        time.sleep(0.1)
+        time.sleep(0.01)
         # should have this method because it inherits from LpgbtV1 which inherits from Lpgbt
-        v1 = lpgbt.read_reg(0x1c5)
 
-        print("v1 is", v1)
-        if v1 == 0xa6:
-            break
-        else:
-            print("trying again...")
-            time.sleep(0.3)
-    except:
-        print("failed but gonna try again")
-        time.sleep(1)
+        print("attempting to read!!")
+        v1 = lpgbt.read_reg(0x1d7)
+        # print("0x{0:x}".format(
+        #     lpgbt.lpgbt_cont_.read_lpgbt_regs(int("0x1d9", 0), 1)[0])
+        # )
+        break
+        # print("v1 is", v1)
+        # if v1 == 0xa6:
+        #     break
+        # else:
+        #     print(f"trying again... it was read as: {v1}")
+        #     time.sleep(0.3)
+    except Exception as e:
+        print(f"failed but gonna try again: {e}")
+        lpgbt.lpgbt_cont_.lpgbt_com.reset_slow_control_logic()
