04bb51c5535ac525e1eb33f1604feba43ab9f43a
Naomi Gonzalez <ngonzalz@bu.edu>
2025-08-25 20:49:07 +0200

    etroc start code

--- etroc_controller.py
+++ etroc_controller.py
@@ -13,46 +13,110 @@ from dataclasses import dataclass
 @dataclass
 class Pixel:
 
+    # Hayden
+    write()
+
+    # Hayden
+    read()
+
 
 class etroc_chip:
+    addr_i2c: int
+    lpgbt: lpgbt_chip
+    connected: bool
+    vref: bool
 
-    def __init__(self, lpgbt: lpgbt_chip):
-        print("init")
 
-        # Check Connectivity: Read first ETROC register (0x2c)
-        # Load register names and address (read csv)
+    def __init__(self, lpgbt: lpgbt_chip, address_i2c: int, ):
+        """
+        Checks connectivity then writes initial configuration of ETROC 
+        """
+        self.lpgbt = lpgbt
+        self. addr_i2c = address_i2c
+        self.i2c_write = partial(
+            self.lpgbt.i2c_master_write,
+            master_id=2,          
+            slave_address=address_i2c,  
+            reg_address_width=8,      
+            timeout=10                    
+        )
+        self.is_connected()
+
+        # HAYDEN
         # initialize pixel matrix
         
         self.DAC_min = 600 #mV
         self.DAC_max = 1000 #mV
         self.DAC_step = 400/2**10
 
-        # Perform hard reset
-        # Power up vref 
-        # config
+        self.reset(hard = True)
+        self.power_Vref(True) 
+        self.config()
 
-    def config(self):
-        print("config")
+
+    # TODO: change this to set/getter
+    def is_connected():
+        """
+        Check ETROC connectivity through reading first reg and verifying
+        value is 0x2c
+        """
+        try:
+            test = self.read(0x0)
+            self.connected = True if test==0x2c else False
+        except TimeoutError:
+            self.connected = False
+        return self.connected
+
 
     def reset(self, hard=False):
-        print("reset")
-        `
-    def power_up_Vref(self):
-        print("power up")
+        if hard:
+            self.lpgbt.write_gpio_output("RESET1",0)
+            sleep(0.05)
+            self.lpgbt.write_gpio_output("RESET1",1)
+        else:
+            self.write("asyResetGlobalReadout", 0)
+            sleep(0.05)
+            self.write("asyResetGlobalReadout", 1)
 
-    def power_down_Vref(self):
-        print("power down")
+
+    def config(self):
+        if not self.is_connected():
+            raise ConnectionError(f"ETROC addr: {self. addr_i2c} Not Connected")
+
+        self.reset()
+        self.write("singlePort", 0)         # use both ports
+        self.write("mergeTriggerData", 1)   # merge trigger and data
+        self.write("disScrambler", 1)       # disable scrambler
+        self.write("serRateRight", 0)       # right port 320Mbps rate
+        self.write("serRateLeft", 0)        # left port 320Mbps rate
+
+        for ...
+            for ...
+                self.pixel[i,j].write("qnji", 0)
 
 
+    # TODO: change this to set/getter
+    def power_Vref(self, val: bool):
+        self.write("VRefGen_PD", val)
+
+    # HAYDEN
     # Implement either number or name of reg
-    def write(self, reg, value):
+    def write(self, register, value):
         print("writing")
 
+
+        # if they give me number
+        reg = PheriphralReg[register]
+        for adr, val in zip(reg.addresses, reg.split_value(value):
+            full_address = stuff & adr
+            self.i2c_write(...)
+
+    # HAYDEN
     def read(self, reg):
         print("reading")
 
+    # HAYDEN
     def autothreshold_scan(self):
-
 
 
 
